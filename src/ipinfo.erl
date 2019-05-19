a0e6ad46c44d13691bbfeec1edaf0f809db23075
Uman Shahzad <uman@mslm.me>
2019-05-20 01:43:07 +0500

    Add some comments describing the lib

--- src/ipinfo.erl
+++ src/ipinfo.erl
@@ -1,5 +1,9 @@
 %%%====================================================================
-%% API functions
+%% IPinfo API client functions.
+%%
+%% Note that nothing here runs through a separate process, OTP style.
+%% These are just library functions that return structured data.
+%% You'll need to utilize them in your own asynchronous processes.
 %%%====================================================================
 
 -module(ipinfo).
